753f92e47cfb392f3fa3ba7606f0e6144e82be90
Calazhen <henriquecalazans.96@outlook.com>
2023-01-16 10:12:41 -0300

    Adicionando métodos GET nos Testes de API em Cypress

---
+++ cypress/e2e/custumers.cy.js
@@ -0,0 +1,84 @@
+describe('empty spec', () => {
+  const baseURL = 'localhost:8080'
+  it('GET Custumer', () => {
+    cy.request ({
+      method: 'POST',
+      url: baseURL + '/cliente',
+      body:{
+        "id": 11,
+        "idade": 15,
+        "nome": "Henrique",
+        "risco": 0
+      }
+    }).then((Response) => {
+        expect (Response.status).to.eq(201)
+      })
+    
+    cy.request ({
+      method: 'GET',
+      url: baseURL + '/cliente/11',
+
+    }).then((Response) => {
+      expect (Response.status).to.eq(200)
+      expect (Response.body).has.property('nome', 'Henrique','idade',15,'id',11,'risco',0) 
+    })
+  })
+
+  it ('GET all custumers', () =>{
+    cy.request ({
+      method: 'POST',
+      url: baseURL + '/cliente',
+      body:{
+        "id": 11,
+        "idade": 15,
+        "nome": "Henrique",
+        "risco": 0
+      }
+    }).then((Response) => {
+        expect (Response.status).to.eq(201)
+      })
+
+      cy.request ({
+        method: 'POST',
+        url: baseURL + '/cliente',
+        body:{
+          "id": 13,
+          "idade": 26,
+          "nome": "Calazans",
+          "risco": 0
+        }
+      }).then((Response) => {
+          expect (Response.status).to.eq(201)
+        })
+    
+    cy.request ({
+      method: 'GET',
+      url: baseURL + '/cliente/11',
+
+    }).then((Response) => {
+      expect (Response.status).to.eq(200)
+      expect (Response.body).has.property('nome', 'Henrique','idade',15,'id',11,'risco',0,'nome', 'Calazans','idade',26,'id',13,'risco',0) 
+    })
+
+  })
+
+  it('POST a Custumer',()=>{
+    cy.request ({
+      method: 'POST',
+      url: baseURL + '/cliente',
+      body:{
+        "id": 11,
+        "idade": 15,
+        "nome": "Henrique",
+        "risco": 0
+      }
+    }).then((Response) => {
+ 
+        expect (Response.status).to.eq(201)
+        expect (Response.body).have.property('nome', 'Henrique','idade',15,'id',11,'risco',0) 
+    
+    })
+    
+    })
+    it.only
+})
